18e1047cf94f09e09b0634f0c2b38cad463accc4
rictorwin <rictor@gmail.com>
2022-07-22 10:36:16 -0500

    Agregando archivo index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <nav></nav>
+    <section>
+       <h2>Página de inicio</h2>
+       <hr />
+       <p>Párrafo principal de la página de inicio</p>
+    </section>
+    <footer>
+       <p>Derechos reservador @rctorr</p>
+    </footer>
+</body>
+</html>
+
